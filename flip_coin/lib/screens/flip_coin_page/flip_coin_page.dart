08101656ac3717c9000ad9aca63760657f5ecee9
Lemuel Manske <lemuelkaue@gmail.com>
2022-03-03 09:18:30 -0300

    Testando Rive animations

--- flip_coin/lib/screens/flip_coin_page/flip_coin_page.dart
+++ flip_coin/lib/screens/flip_coin_page/flip_coin_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
+import 'package:flutter/services.dart';
+import 'package:rive/rive.dart';
 
 class FlipCoinPage extends StatefulWidget {
   const FlipCoinPage({ Key? key }) : super(key: key);
@@ -8,24 +9,55 @@ class FlipCoinPage extends StatefulWidget {
   _FlipCoinPageState createState() => _FlipCoinPageState();
 }
 
-class _FlipCoinPageState extends State<FlipCoinPage> {
+class _FlipCoinPageState extends State<FlipCoinPage> { 
+  late Artboard _artboard;
+  late RiveAnimationController _controller;
+
   @override
-  Widget build(BuildContext context) {    
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Lottie Test"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              child: Lottie.asset(
-                  'assets/coin.json'),
-            ),
-          ],
-        ),
-      ),
+  void initState() {
+    _loadRiveFile();
+    super.initState();
+  }
+
+  void _loadRiveFile() async {
+    final bytes = await rootBundle.load('assets/coinflip.riv');
+    final file = RiveFile.import(bytes);
+
+    setState(() {
+      _artboard = file.mainArtboard;
+    });
+  }
+
+  void _coinFlipReturnS() async {
+    _artboard.addController(
+      _controller = SimpleAnimation('CoinFlipReturnS'),
+    );
+    setState(() => _controller.isActive = true);
+  }
+
+  void _coinFlipReturnC() async {
+    _artboard.addController(
+      _controller = SimpleAnimation('CoinFlipReturnC'),
+    );
+    setState(() => _controller.isActive = true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height - 250,
+            child: _artboard != null
+                ? Rive(
+                    artboard: _artboard,
+                    fit: BoxFit.cover,
+                  )
+                : Container()),
+        TextButton(onPressed: () => _coinFlipReturnS(), child: Text('Return C')),
+        TextButton(onPressed: () => _coinFlipReturnC(), child: Text('Return S'))
+      ],
     );
   }
 }
