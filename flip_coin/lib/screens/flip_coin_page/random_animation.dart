c28727d00093ba05cdc3f1fe3f0f4bccaf0ae9d9
Lemuel Manske <lemuelkaue@gmail.com>
2022-03-03 17:19:37 -0300

    Atualizando animações .riv

--- flip_coin/lib/screens/flip_coin_page/random_animation.dart
+++ flip_coin/lib/screens/flip_coin_page/random_animation.dart
@@ -1,22 +1,11 @@
 import 'dart:math';
 
-late String retornoRandomAnimation;
-
-String initAnimation() {
-  int randomNumber = Random().nextInt(2); // 1 => Cara; 2 => Coroa
-  if(randomNumber == 1) {
-    retornoRandomAnimation = 'CoinFlipStartsSReturnS';
-  } else {
-    retornoRandomAnimation = 'CoinFlipStartsSReturnC';
-  } return retornoRandomAnimation;
-}
-
 String randomAnimation() {
-  initAnimation.call();
-  int randomNumber = Random().nextInt(2); // 1 => Cara; 2 => Coroa
-  if(retornoRandomAnimation == 'CoinFlipStartsSReturnC' && randomNumber == 2) {
-    return 'CoinFlipStartsCReturnC';
-  } else{
-    return 'CoinFlipStartsCReturnS';
-  }}
+  int firstRandomNumber = Random().nextInt(2); // 1 => Cara; 2 => Coroa
 
+  if (firstRandomNumber == 1) {
+    return 'CoinFlipStartsSReturnC';
+  } else {
+    return 'CoinFlipStartsSReturnS';
+  }
+}
