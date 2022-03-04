5d583ccd5bdb77cbde3cb1581846af561f2028b9
Lemuel Manske <lemuelkaue@gmail.com>
2022-03-03 23:09:32 -0300

    Nova animação .riv

--- flip_coin/lib/screens/flip_coin_page/flip_coin_page.dart
+++ flip_coin/lib/screens/flip_coin_page/flip_coin_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 import 'package:flip_coin/screens/flip_coin_page/random_animation.dart';
+import 'package:flip_coin/screens/default.dart';
 
 class FlipCoinPage extends StatefulWidget {
   const FlipCoinPage({ Key? key }) : super(key: key);
@@ -11,20 +12,42 @@ class FlipCoinPage extends StatefulWidget {
 
 class _FlipCoinPageState extends State<FlipCoinPage> { 
   late RiveAnimationController _startAnimationController;
+  late RiveAnimationController _restartAnimationController;
 
-  bool _isPlaying = false;
+  bool _isAnimationPlaying = false;
+  bool _isButtonVisible = true;
+  bool _isRestartButton = false;
+
+  var currentAnimation = randomAnimation();
 
   @override
   void initState() {
     super.initState();
     _startAnimationController = OneShotAnimation(
-      randomAnimation(),
+      currentAnimation,
       autoplay: false,
       onStop: () => setState(() {
-        _isPlaying = false;
+        _isAnimationPlaying = false;
+        _isButtonVisible = true;
+        if (currentAnimation == 'CoinFlipStartsSReturnC') {
+          _isRestartButton = true;
+        }
       }),
       onStart: () => setState(() { 
-        _isPlaying = true;      
+        _isAnimationPlaying = true;
+        _isButtonVisible = false;
+      }),
+    );
+
+    _restartAnimationController = OneShotAnimation(
+      'RestartFromCToS',
+      autoplay: false,
+      onStop: () => setState(() {
+        _isButtonVisible = true;
+        _isRestartButton = false;
+      }),
+      onStart: () => setState(() { 
+        _isButtonVisible = false;
       }),
     );
   }
@@ -43,14 +66,46 @@ class _FlipCoinPageState extends State<FlipCoinPage> {
               SizedBox(
                 width: 370,
                 height: 370,
-                child: GestureDetector(
-                  onTap: () => _isPlaying ? null : _startAnimationController.isActive = true,
-                  child: RiveAnimation.asset(
-                    'assets/coinflip.riv',
-                    controllers: [_startAnimationController],
-                  ),
+                child: RiveAnimation.asset(
+                  'assets/coinflip.riv',
+                  controllers: [_startAnimationController, _restartAnimationController],
                 ),
               ),
+            ],
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedOpacity(
+                opacity: _isButtonVisible ? 1.0 : 0.0,
+                duration: const Duration(milliseconds: 500),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: buttonColor,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30))
+                    ),
+                    fixedSize: buttonSize,
+                  ),
+              
+                  onPressed: () {
+                    setState(() {
+                      if (_isRestartButton) {
+                        _restartAnimationController.isActive = true;
+                      } else {
+                        _startAnimationController.isActive = true;
+                      }
+                    });
+                  },
+
+                  child: Icon(
+                    _isRestartButton? Icons.restart_alt_rounded: Icons.play_arrow_rounded,
+                    color: Colors.white,
+                    size: 100,
+                  ),
+                ),
+              )
             ],
           ),
         ]
--- flip_coin/lib/screens/flip_coin_page/random_animation.dart
+++ flip_coin/lib/screens/flip_coin_page/random_animation.dart
@@ -4,8 +4,8 @@ String randomAnimation() {
   int firstRandomNumber = Random().nextInt(2); // 1 => Cara; 2 => Coroa
 
   if (firstRandomNumber == 1) {
-    return 'CoinFlipStartsSReturnC';
-  } else {
     return 'CoinFlipStartsSReturnS';
+  } else {
+    return 'CoinFlipStartsSReturnC';
   }
 }
