b6a7429481d38b540d090698bfed6f9f8a20c7ab
Lemuel Manske <lemuelkaue@gmail.com>
2022-03-07 01:03:13 -0300

    Aplicando novas animações .riv

--- flip_coin/lib/screens/flip_coin_page/flip_coin_page.dart
+++ flip_coin/lib/screens/flip_coin_page/flip_coin_page.dart
@@ -1,8 +1,7 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:rive/rive.dart';
-import 'package:flip_coin/screens/default.dart';
+
 
 class FlipCoinPage extends StatefulWidget {
   const FlipCoinPage({ Key? key }) : super(key: key);
@@ -12,107 +11,99 @@ class FlipCoinPage extends StatefulWidget {
 }
 
 class _FlipCoinPageState extends State< FlipCoinPage> { 
-  SMIInput<bool>? _PlayInput;
-  SMIInput<bool>? _RestartInput;
-  SMIInput<double>? _randomNumberInput;
-  Artboard? _coinFlipArtboard;
-  
-  bool _isButtonVisible = true;
+  // CoinFlipAnimation Inputs
+  SMITrigger? _PlayInput;
+  SMITrigger? _RestartInput;
+  SMINumber? _randomNumberInput;
+
+  // PlayPauseButtonAnimation Inputs
+  SMIBool? _isPlayingInput; 
+
   bool _isRestartButton = false;
+
+  // CoinFlipAnimationInit
+  void _onCoinFlipInit(Artboard artboard) {
+    final controller = StateMachineController.fromArtboard(
+      artboard, 
+      'CoinFlip',
+    );
+    artboard.addController(controller!);
+
+    _PlayInput = controller.findInput<bool>('Play') as SMITrigger;
+    _RestartInput = controller.findInput<bool>('Restart') as SMITrigger;
+    _randomNumberInput = controller.findInput<double>('RandomNumber') as SMINumber;
+  }
 
   void _playCoinFlipAnimation() {
     _randomNumberInput?.value = (1 + Random().nextInt(2).toDouble());
-
-    if (_PlayInput?.value == false &&
-        _PlayInput?.controller.isActive == false &&
-        _RestartInput?.value == false &&
-        _RestartInput?.controller.isActive == false) {
-      _PlayInput?.value = true; 
-    } else if (_PlayInput?.value == true &&
-               _PlayInput?.controller.isActive == false &&
-               _RestartInput?.value == false &&
-               _RestartInput?.controller.isActive == false) {
-      _PlayInput?.value = false;
-      _RestartInput?.value = true;
-    } else if (_PlayInput?.value == false &&
-               _PlayInput?.controller.isActive == false &&
-               _RestartInput?.value == true &&
-               _RestartInput?.controller.isActive == false) {
-      _RestartInput?.value = true;
-      _PlayInput?.value = false;
+    
+    if (_isRestartButton == false) {
+      _PlayInput?.fire();
+    } else {
+      _RestartInput?.fire();
     }
   }
 
-  @override
-  void initState() {
-    super.initState();
-    rootBundle.load('assets/coinflip.riv').then((data) {
-      final file = RiveFile.import(data);
-      final artboard = file.mainArtboard;
+  // PlayPauseButtonInit
+  void _onPlayPauseButtonInit(Artboard artboard) {
+    final controller = StateMachineController.fromArtboard(
+      artboard, 
+      'PlayPauseButton',
+    );
+    artboard.addController(controller!);
 
-      var controller = StateMachineController.fromArtboard(
-        artboard,
-        'CoinFlip',
-      );
-
-      if (controller != null) {
-        artboard.addController(controller);
-        _PlayInput = controller.findInput('Play');
-        _RestartInput = controller.findInput('Restart');
-        _randomNumberInput = controller.findInput('RandomNumber');
-      }
-      setState(() => _coinFlipArtboard = artboard);
-    });
+    _isPlayingInput = controller.findInput<bool>('isPlaying') as SMIBool;
   }
 
+  void _playPauseButtonAnimation() {
+    if (_isPlayingInput?.value == false &&
+        _isPlayingInput?.controller.isActive == false) {
+      _isPlayingInput?.value = true;
+      _isRestartButton = false;
+    } else if (_isPlayingInput?.value == true &&
+        _isPlayingInput?.controller.isActive == false) {
+      _isPlayingInput?.value = false;
+      _isRestartButton = true;
+    }
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _coinFlipArtboard == null
-          ? const SizedBox()
-          : Row(
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SizedBox(
                 height: 370,
                 width: 370,
-                child: Rive(
-                  artboard: _coinFlipArtboard!,
-                  fit: BoxFit.fitHeight,
-                ),
+                child: RiveAnimation.asset(
+                  'assets/coinflip.riv',
+                  fit: BoxFit.cover,
+                  onInit: _onCoinFlipInit,
+                )
               ),
             ],
           ),
 
-          AnimatedOpacity(
-            opacity: _isButtonVisible ? 1.0 : 0.0,
-            duration: const Duration(milliseconds: 500),
-            child: TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: buttonColor,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(30))
-                ),
-                fixedSize: buttonSize,
-              ),
-          
-              onPressed: () {
-                setState(() {
-                  _isButtonVisible = false;
-                  _playCoinFlipAnimation();
-                });
-              },
-
-              child: Icon(
-                _isRestartButton? Icons.restart_alt_rounded: Icons.play_arrow_rounded,
-                color: Colors.white,
-                size: 100,
+          GestureDetector(
+            child: SizedBox(
+              height: 150,
+              width: 150  ,
+              child: RiveAnimation.asset(
+                'assets/playpausebutton.riv',
+                fit: BoxFit.cover,
+                onInit: _onPlayPauseButtonInit,
               ),
             ),
-          )
+            onTap: () {
+              setState(() {
+                _playCoinFlipAnimation();
+                _playPauseButtonAnimation();
+              });
+            })
         ]
       )
     );
