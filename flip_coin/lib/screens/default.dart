0734fd4895670e193c58a78af88a5908d6dea0db
Lemuel Manske <lemuelkaue@gmail.com>
2022-03-07 18:20:49 -0300

    Alterando cor de fundo do app

--- flip_coin/lib/screens/default.dart
+++ flip_coin/lib/screens/default.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 const Color buttonColor = Color(0xFFFFD500);
-const Color mainAppColor = Color(0xFFFFFEEE);
+const Color mainAppColor = Color.fromARGB(255, 255, 252, 218);
 const Size buttonSize = Size(130, 130);
