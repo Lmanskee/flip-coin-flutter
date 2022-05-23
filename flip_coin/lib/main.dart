4faa1decd1ad1ee57a04b615dbf420cec8238827
Lemuel Manske <lemuelkaue@gmail.com>
2022-05-23 17:57:47 -0300

    Removendo google_mobile_ads

--- flip_coin/lib/main.dart
+++ flip_coin/lib/main.dart
@@ -8,11 +8,9 @@ import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 
-import 'package:google_mobile_ads/google_mobile_ads.dart'; 
 
 void main() { 
   WidgetsFlutterBinding.ensureInitialized();
-  MobileAds.instance.initialize();
 
   runApp(
     MultiProvider(
